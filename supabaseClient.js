976527a000222c72614c39227ecd6e5846be332f
ganeshveerapur35-dev <ganeshveerapur35@gmail.com>
2026-03-30 22:43:19 +0530

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://abaxkoqbjkchimawjmrp.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiYXhrb3FiamtjaGltYXdqbXJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NTg2NTMsImV4cCI6MjA5MDQzNDY1M30.Sv_pjeJVfiz-rwk72OViyN3JNUsOJEPb8l2khXGgS3o'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
